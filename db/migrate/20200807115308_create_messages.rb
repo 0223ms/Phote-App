54f1003ab2e86571c5b64e6108bfdeb2ef795645
YourName <you@example.com>
2020-08-07 21:42:55 +0900

    Update 20200807115308_create_messages.rb

--- db/migrate/20200807115308_create_messages.rb
+++ db/migrate/20200807115308_create_messages.rb
@@ -3,8 +3,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
       t.string :content
       t.string :image
-      t.references :user, null: false, type: :bigint, foreign_key: true
-      t.references :room, null: false, type: :bigint, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :room, null: false, foreign_key: true
       t.timestamps
     end
   end
