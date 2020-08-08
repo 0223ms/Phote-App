5fbe4154ed109ea8e172b7969f36d2bd4623a2e1
akkun016 <nakataakito@icloud.com>
2020-08-08 19:16:48 +0900

    Update 20200807115308_create_messages.rb

--- db/migrate/20200807115308_create_messages.rb
+++ db/migrate/20200807115308_create_messages.rb
@@ -3,8 +3,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
       t.string :content
       t.string :image
-      t.references :user, null: false, foreign_key: true
-      t.references :room, null: false, foreign_key: true
+      t.bigint :user_id, null: false, foreign_key: true
+      t.bigint :room_id, null: false, foreign_key: true
       t.timestamps
     end
   end
