1af7d0243d063723f8db72a93f65d3455cad8f2a
YourName <you@example.com>
2020-08-09 23:14:20 +0900

    post-page

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
   root "tops#index"
 
   resources :messages, only: [:index, :show]
+
+  resources :posts, only: [:index, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
