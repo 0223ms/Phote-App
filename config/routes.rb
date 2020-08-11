15d2de220edd1e62bfc562854854bb31f81460d2
YourName <you@example.com>
2020-08-11 20:43:45 +0900

    tag-page

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
   resources :messages, only: [:index, :show]
 
   resources :posts, only: [:new]
+  resources :tags, only: [:new]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
