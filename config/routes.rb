3507a69be669421f3503f39457329ae39b0f2509
YourName <you@example.com>
2020-08-11 12:29:13 +0900

    posts index

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   resources :messages, only: [:index, :show]
 
-  resources :posts, only: [:index, :new]
+  resources :posts, only: [:new]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
