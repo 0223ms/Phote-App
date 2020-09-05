25a3598a0d4ac082074202f850cbb88e94b5c1b9
YourName <you@example.com>
2020-09-06 00:39:48 +0900

    update

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,12 +1,9 @@
 $(document).on('turbolinks:load', function() {
   $(function() {
     $(".option-btn-post").on('click', function(){
-    console.log(this);
       var content = $(this).next(".gray-back");
-      console.log(content);
       content.fadeIn("show");
       var contentMenu = content.children(".option-menu");
-      console.log(contentMenu);
       contentMenu.fadeIn("show");
     })
   });
