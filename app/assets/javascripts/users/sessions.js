111703e2072676fe80663b0def6cea5773881b4a
akkun016 <nakataakito@icloud.com>
2020-09-07 23:30:32 +0900

    ロード画面の作成、ログイン、新規登録ページのデザイン変更

--- app/assets/javascripts/users/sessions.js
+++ app/assets/javascripts/users/sessions.js
@@ -1,23 +1,5 @@
 $(document).on('turbolinks:load', function() {
   $(function(){
-    $(".btn-app").hover(
-      function(){
-        $(this).css('opacity', '0.5');
-      },
-      function(){
-        $(this).css('opacity', '');
-      }
-    );
-  
-    $(".btn-google").hover(
-      function(){
-        $(this).css('opacity', '0.5');
-      },
-      function(){
-        $(this).css('opacity', '');
-      }
-    );
-
     $("#signIp-form-field-email, #signIp-form-field-password").on('keyup', function(){
       let email = $("#signIp-form-field-email");
       let password = $("#signIp-form-field-password");
