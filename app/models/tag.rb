895f5568d81ae8399cd3fc2af261cb1d98b99686
YourName <you@example.com>
2020-09-16 22:45:40 +0900

    Update tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :tag_posts, dependent: :destroy
+  has_many :tag_posts, dependent: :delete_all
   has_many :posts, through: :tag_posts
 
   validates :name, presence: true, length: {maximum:99}
