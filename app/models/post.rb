1bbf6e147ca389435c9e3e2fcc49ae0803c39f7c
YourName <you@example.com>
2020-09-16 22:45:46 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   belongs_to :user
-  has_many :tag_posts, dependent: :destroy
+  has_many :tag_posts, dependent: :delete_all
   has_many :tags, through: :tag_posts
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
