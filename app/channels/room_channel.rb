c7a2916e97282cd1e15d36d2d94d10f8e8efe797
akkun016 <nakataakito@icloud.com>
2020-11-01 23:36:33 +0900

    メッセージ機能の修正

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -8,12 +8,16 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    before_time = Message.where(room_id: data['room_id']).last.created_at.strftime("%Y年%-m月%-d日 %-H:%M")
+    unless Message.where(room_id: data['room_id']).empty?
+      before_time = Message.where(room_id: data['room_id']).last.created_at.strftime("%Y年%-m月%-d日 %-H:%M")
+    end
     message = Message.create!(content: data['content'], user_id: data['user_id'], room_id: data['room_id'])
     time = message.created_at.strftime("%Y年%-m月%-d日 %-H:%M")
     data['time'] = time
     data['current_user_id'] = current_user.id
-    data['before_time'] = before_time
+    unless before_time.blank?
+      data['before_time'] = before_time
+    end
     ActionCable.server.broadcast 'room_channel', data
   end
 end
